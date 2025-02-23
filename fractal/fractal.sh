6b66480053d23dc5f8a98b0c59c602811800b6c3
bmzsombi <bmzsombi@sch.bme.hu>
2025-02-23 20:12:41 +0100

    asd

--- fractal/fractal.sh
+++ fractal/fractal.sh
@@ -3,7 +3,10 @@
 # May be modified
 
 #SBATCH -o fractal.out
+#SBATCH --job-name=mandelbrot
+#SBATCH --nodes=2              # Az igényelt node-ok száma
+#SBATCH --ntasks-per-node=4    # Az egyes node-okon futó processzorok száma
+#SBATCH --time=01:00:00        # Maximális futási idő
 
-
-./fractal
+srun --mpi=pmi2 ./fractal
 
